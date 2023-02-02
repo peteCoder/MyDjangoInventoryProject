3dde753e9fddf70e89756665ba68b43cd43dc6a5
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-02-02 01:05:01 +0100

    final project

--- ovbioise/settings.py
+++ ovbioise/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
 
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 
